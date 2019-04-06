c82ae4435f37e8be816231cae48ac221f849961b
Nicole-Packard <nicole.packard23@gmail.com>
2019-04-05 21:14:13 -0500

    Update LearningComputer.java
    Comments added; no change.

--- LearningComputer.java
+++ LearningComputer.java
@@ -1,5 +1,11 @@
+import java.util.Random;
+
 package tic-tac-toe;
 
 class LearningComputer extends Player {
-	
+	//Initialize fileInputReader
+	//Constructor: writes token placement odds into array
+	//OVERRIDE: Place token based on rng + past winnings knowledge
+	//Initialize fileOutputWriter
+	//Output writes game data of the winner to a file
 }
